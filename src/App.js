5ceb59b438f869ba6164196c613e5dcc6bac67ef
taras <nazarko94@gmail.com>
2022-12-19 13:19:45 +0200

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, {Component} from 'react'
-import Layout from './hoc/Layout/Layout'
-import Quiz from './containers/Quiz/Quiz'
+import React, {Component} from 'react';
+import { Routes, Route } from "react-router-dom";
+import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
+import QuizList from './containers/QuizList/QuizList';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Routes>
+          <Route path='/quiz:id' element={<Quiz/>}/>
+          <Route path='/auth' element={<Auth/>}/>
+          <Route path='/quiz-creator' element={<QuizCreator/>}/>
+          <Route path='/' element={<QuizList/>}/>
+        </Routes>
       </Layout>
     )
   }
 }
 
-export default App
+export default App;
--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,18 +1,36 @@
 import React, {Component} from "react";
 import classNames from "classnames";
-import classes from './Drawer.css';
 import './Drawer.css';
+import { NavLink } from "react-router-dom";
 import Backdrop from "../../UI/Button/Backdrop/Backdrop";
 
-const links = [1, 2, 3,];
+const links = [
+  {to: '/', label: 'List'},
+  {to: '/auth', label: 'Signup'},
+  {to: '/quiz-creator', label: 'Create test'},
+];
 
 class Drawer extends Component {
+  handleClick = () => {
+    this.props.onClose()
+  }
 
   renderLinks() {
+    let activeClass = classNames('active');
+
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            className={({isActive}) => 
+              isActive ? activeClass : null
+            }
+            onClick={this.handleClick}
+          >
+            {link.label}
+          </NavLink>
         </li>
       )
     })
